47b78e7e1f5f5aefee1bd54ce495bb986f5b7464
Saman Arshad <saman.arshad97@gmail.com>
2021-03-06 23:39:25 +0530

    Updates

---
+++ precache-manifest.36d11078d8a19f9ab221b8aa83abfde5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cba003652ab17c01d411ca5fc35d170",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a32f7a46a53b27f5df89",
+    "url": "/portfolio/static/css/main.875358a9.chunk.css"
+  },
+  {
+    "revision": "ca841331574b82eb9cb7",
+    "url": "/portfolio/static/js/2.ad2a6b96.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/portfolio/static/js/2.ad2a6b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32f7a46a53b27f5df89",
+    "url": "/portfolio/static/js/main.38a533d2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "2cee5a4ba721976ae5e71d5270fb56d6",
+    "url": "/portfolio/static/media/SamanResume.2cee5a4b.pdf"
+  },
+  {
+    "revision": "1a814050198759ff0cf26d814b8f1068",
+    "url": "/portfolio/static/media/api.1a814050.svg"
+  },
+  {
+    "revision": "dcfce956ad295548dbed1a1436d4cb79",
+    "url": "/portfolio/static/media/computer.dcfce956.svg"
+  },
+  {
+    "revision": "301c6922774812893b2c73fe841dc101",
+    "url": "/portfolio/static/media/facebook.301c6922.svg"
+  },
+  {
+    "revision": "8e67bdf65f0d071c44ccadd06a499b27",
+    "url": "/portfolio/static/media/github.8e67bdf6.svg"
+  },
+  {
+    "revision": "52f7fbfe1a9f0caad2c64406bc14d962",
+    "url": "/portfolio/static/media/instagram.52f7fbfe.svg"
+  },
+  {
+    "revision": "fb06f957d205389c95de4cd7a6e5554a",
+    "url": "/portfolio/static/media/linkedin.fb06f957.svg"
+  },
+  {
+    "revision": "7e208117330ae63da50b02d8a8bbeeb4",
+    "url": "/portfolio/static/media/linkyshare.7e208117.png"
+  },
+  {
+    "revision": "c7d108245c2a06b2e15adfc32d3a8785",
+    "url": "/portfolio/static/media/natours.c7d10824.png"
+  },
+  {
+    "revision": "b890dc785d9e1ac526159d8a64ea6c6d",
+    "url": "/portfolio/static/media/nodefarm.b890dc78.png"
+  },
+  {
+    "revision": "212944aeb09f3e81986c5d1d7311bb22",
+    "url": "/portfolio/static/media/pin.212944ae.svg"
+  },
+  {
+    "revision": "1a645b5c1fcb928f30b1679edb18f980",
+    "url": "/portfolio/static/media/profile.1a645b5c.jpg"
+  },
+  {
+    "revision": "533aa761f2638acb5a3c5cddf9fc1745",
+    "url": "/portfolio/static/media/repair.533aa761.svg"
+  },
+  {
+    "revision": "dcaf0bba1942161da55d026a04173b83",
+    "url": "/portfolio/static/media/software.dcaf0bba.svg"
+  },
+  {
+    "revision": "57dec70492451ba325246fde8c20db5a",
+    "url": "/portfolio/static/media/tie.57dec704.svg"
+  },
+  {
+    "revision": "cc15bbaca98ebc94471f52d68a1ee5c0",
+    "url": "/portfolio/static/media/todoapp.cc15bbac.png"
+  },
+  {
+    "revision": "fcddb395a15ab92320343194d09a239a",
+    "url": "/portfolio/static/media/twitter.fcddb395.svg"
+  }
+]);
