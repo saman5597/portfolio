d5d58c6233da25ef27428dd3035603b7dfd5ad1f
Saman Arshad <saman@agvahealthcare.com>
2021-03-25 11:10:32 +0530

    Added new project data

--- src/components/data/project_data.js
+++ src/components/data/project_data.js
@@ -2,28 +2,15 @@ import linkyshare from '../../assets/projects_images/linkyshare.png'
 import natours from '../../assets/projects_images/natours.png'
 import nodefarm from '../../assets/projects_images/nodefarm.png'
 import todoapp from '../../assets/projects_images/todoapp.png'
+import instaclone from '../../assets/projects_images/instaclone.png'
 
 const data_projects = [
     {
-        name: "LinkyShare",
-        image: linkyshare,
-        deployedUrl: "https://linkyshare.herokuapp.com/",
-        githubUrl: "https://github.com/saman5597/linkyshare",
-        category: ['NodeJS, MongoDB']
-    },
-    {
-        name: "Natours",
-        image: natours,
-        deployedUrl: "https://natours-saman.herokuapp.com/",
-        githubUrl: "https://github.com/saman5597/natours",
-        category: ['NodeJS, MongoDB']
-    },
-    {
-        name: "Node Farm",
-        image: nodefarm,
-        deployedUrl: "https://node-farmm.herokuapp.com/",
-        githubUrl: "https://github.com/saman5597/node-farm",
-        category: ['NodeJS']
+        name: "InstaClone",
+        image: instaclone,
+        deployedUrl: "https://instaclone597.herokuapp.com/",
+        githubUrl: "https://github.com/saman5597/instaclone",
+        category: ['React', 'NodeJS', 'MongoDB']
     },
     {
         name: "To Do App",
@@ -31,6 +18,27 @@ const data_projects = [
         deployedUrl: "https://saman-todo.netlify.app/",
         githubUrl: "https://github.com/saman5597/react-todo",
         category: ['React']
+    },
+    {
+        name: "LinkyShare",
+        image: linkyshare,
+        deployedUrl: "https://linkyshare.herokuapp.com/",
+        githubUrl: "https://github.com/saman5597/linkyshare",
+        category: ['NodeJS', 'MongoDB']
+    },
+    {
+        name: "Natours",
+        image: natours,
+        deployedUrl: "https://natours-saman.herokuapp.com/",
+        githubUrl: "https://github.com/saman5597/natours",
+        category: ['NodeJS', 'MongoDB']
+    },
+    {
+        name: "Node Farm",
+        image: nodefarm,
+        deployedUrl: "https://node-farmm.herokuapp.com/",
+        githubUrl: "https://github.com/saman5597/node-farm",
+        category: ['NodeJS']
     }
 ]
 
